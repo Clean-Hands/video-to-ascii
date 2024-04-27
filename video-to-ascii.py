bf4d9e9836bcfcc29a45288fe34b3ecc3f4aebe6
Lazuli <34384401+Clean-Hands@users.noreply.github.com>
2024-04-27 18:18:41 -0500

    Cleaned up code, improved QOL
    Copied the lines of code that keeps track of which frame we should be on to the "play generated frames" section.

--- video-to-ascii.py
+++ video-to-ascii.py
@@ -1,11 +1,18 @@
+print("Importing libraries...")
+
 import os
 from sys import argv
 import cv2
-import pygame
 from moviepy.editor import VideoFileClip
 import tempfile
 import curses
 from time import sleep, perf_counter, time
+
+# prevent the pygame splash text from printing before we import it 
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+
+print("Imported!")
 
 def to_symbol(value):
     mid = value/255
@@ -14,16 +21,44 @@ def to_symbol(value):
     g = len(usable_chars) * mid
     return usable_chars[int(g)]
 
-if len(argv) < 2:
-    print("Incorrect number of arguments.")
+def print_usage_statement():
+    print("\nUsage: video-to-ascii.py -[modifiers] [file]\n\n" +
+          
+          "Modifiers:\n" +
+          "    g: Generate ascii frames from video file [file].\n" +
+          "    p: Load and play frames saved in text file [file].\n" +
+          "    gp: Generate and play frames from video file [file].\n" +
+          "    r: Real-time render and display frames from video file [file].\n" +
+          "    ra: Real-time render and display frames from video file [file] with audio.\n")
     exit(1)
 
-dark_to_light = " ·:;!iIH#N@"[::-1]
-usable_chars = [dark_to_light[::-1][i] for i in range(len(dark_to_light))]
+if len(argv) < 3:
+    print_usage_statement()
+
+dark_to_light = " ·:;!iIH#N@"
+usable_chars = [dark_to_light[i] for i in range(len(dark_to_light))]
+
+
+if argv[1][0] !=  "-" or len(argv[1]) < 2 or len(argv[1]) > 3:
+    print_usage_statement()
+else:
+    want_generate = "g" in argv[1]
+    want_play = "p" in argv[1]
+    want_realtime = "r" in argv[1]
+    want_audio = "a" in argv[1]
+
+    if not (want_generate or want_play or want_realtime or want_audio):
+        print_usage_statement()
+    if want_audio and ((want_play or want_generate) or not want_realtime):
+        print_usage_statement()
+    if want_play and (want_realtime or want_audio):
+        print_usage_statement()
+    if want_generate and want_realtime:
+        print_usage_statement()
 
 
 
-if argv[1] == "-r":
+if want_realtime:
 
     if len(argv) < 3:
         print("Incorrect number of arguments.")
@@ -35,11 +70,13 @@ if argv[1] == "-r":
         print(f"Video \"{video}\" does not exist.")
         exit(1)
 
-    cam = cv2.VideoCapture(video)
-    fps = cam.get(cv2.CAP_PROP_FPS)
-    totalFrames = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
+    # load video file
+    cap = cv2.VideoCapture(video)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    totalFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     
-    r, frame = cam.read()
+    # calculate frame height and width
+    r, frame = cap.read()
     vid_height, vid_width = frame.shape[0], frame.shape[1]*2.2
     term_width, term_height = os.get_terminal_size().columns-1, os.get_terminal_size().lines-1
 
@@ -49,65 +86,65 @@ if argv[1] == "-r":
     else:
         height = term_height
         width = int(height*vid_width/vid_height)
-    
-    # Load the video clip
-    video_clip = VideoFileClip(video)
 
-    # Extract the audio from the video
-    audio_clip = video_clip.audio
+    # if the user wants audio, prepare the audio
+    if want_audio:
+        print("EXTRACTING AUDIO:")
 
-    # Save audio to a temporary file
-    temp_audio_file = tempfile.NamedTemporaryFile(suffix=".wav", delete=False)
-    temp_audio_file.close()
-    audio_clip.write_audiofile(temp_audio_file.name)
+        # Load the video clip
+        video_clip = VideoFileClip(video)
 
-    # Initialize Pygame
+        # Extract the audio from the video
+        audio_clip = video_clip.audio
+
+        # Save audio to a temporary file
+        temp_audio_file = tempfile.NamedTemporaryFile(suffix=".wav")
+        temp_audio_file.close()
+        audio_clip.write_audiofile(temp_audio_file.name)
+        audio_clip.close()
+        
+
     pygame.init()
-
-    # Initialize curses
     stdscr = curses.initscr()
 
     try:
-
-        curr_frame = 0
-
-        # Initialize Pygame's mixer module
-        pygame.mixer.init()
-
-        # Load audio file into Pygame's mixer
-        pygame.mixer.music.load(temp_audio_file.name)
-
-        # Play the audio
-        pygame.mixer.music.play()
+        # if the user wants audio, load and play the audio file
+        if want_audio:
+            pygame.mixer.init()
+            pygame.mixer.music.load(temp_audio_file.name)
+            pygame.mixer.music.play()
 
         # use current time to figure out which frame should be shown at any moment
         start_time = time()
 
+        curr_frame = 0
         for i in range(totalFrames):
 
             curr_frame += 1
 
-            ret,frame = cam.read()
+            # read the next frame
+            ret,frame = cap.read()
             if not ret:
                 break
-                
+            
+            # prepare the frame for conversion
             resized = cv2.resize(frame, dsize=(width, height))
             monochromatic = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
 
-            lst2d = [[""]*width for i in range(height)]
+            # convert the frame to ascii characters
+            lst2d = [[""] * width for i in range(height)]
             for x in range(height):
                 for y in range(width):
                     lst2d[x][y] = to_symbol(monochromatic[x][y])
-                    if lst2d[x][y] not in usable_chars:
-                        input(f"{lst2d}")
 
+            # join the lines together
             lines = ""
             for lsty in lst2d:
                 line = "".join(lsty)
-                lines+=line+"\n"
+                lines += line + "\n"
             
+            # display the frame
             stdscr.move(0, 0)
-            
             stdscr.addstr(lines)
             stdscr.refresh()
             
@@ -115,12 +152,11 @@ if argv[1] == "-r":
             # frame based on the time passed and the frame rate
             # i.e. skip frames in order to get back up to speed
             while time() - start_time >= 1/fps * curr_frame:
-                cam.read()
+                cap.read()
                 curr_frame += 1
 
             # if this frame took shorter than 1/fps seconds,
-            # sleep the remaining amount of time
-            # sleep(max(0,(1/fps)-elapsed))
+            # spin for the remaining amount of time
             while time() - start_time <= 1/fps * curr_frame:
                 pass
 
@@ -128,48 +164,47 @@ if argv[1] == "-r":
     finally:
         # Restore the terminal to its original state
         curses.endwin()
-        cam.release()
+        cap.release()
+
+        # delete the temp file
+        if want_audio:
+            pygame.mixer.music.unload()
+            os.remove(temp_audio_file.name)
+            print(f"Deleted temp file {temp_audio_file.name}")
 
 
 else:
 
-    if argv[1] == "-p":
+    if want_generate:
 
-        if len(argv) < 3:
-            print("Incorrect number of arguments.")
-            exit(1)
-
-        if not os.path.exists(argv[2]):
-            print(f"ASCII file \"{argv[2]}\" does not exist.")
-            exit(1)
-
-        f = open(f"{argv[2]}", "r")
-        framesRendered = len(f.readlines()) - 1
-        f.seek(0)
-
-    else:
-
-        video = argv[1]
+        video = argv[2]
 
         if not os.path.exists(video):
             print(f"Video \"{video}\" does not exist.")
             exit(1)
 
-        cam = cv2.VideoCapture(video)
-        fps = cam.get(cv2.CAP_PROP_FPS)
-        totalFrames = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
+        # load video file
+        cap = cv2.VideoCapture(video)
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        totalFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
+        # if the data directory doesn't exist, create it 
         try:
             if not os.path.exists('data'):
                 os.makedirs('data')
         except OSError:
             print("Error Creating Directory of Data")
+        
+        # create output file
+        video_fixed = video.replace(".\\", "")
+        output_file = f".\data\{video_fixed} output.txt"
+        f = open(output_file, "w")
 
-        f = open(f"data/{video} output.txt", "w")
-
+        # save the video's fps within the text file
         f.write(f"FRAME_RATE: {fps}\n")
 
-        r, frame = cam.read()
+        # calculate the frame's height and width
+        r, frame = cap.read()
         vid_height, vid_width = frame.shape[0], frame.shape[1]*2.2
         term_width, term_height = os.get_terminal_size().columns, os.get_terminal_size().lines-1
 
@@ -180,82 +215,108 @@ else:
             height = term_height
             width = int(height*vid_width/vid_height)
 
+
         framesRendered = 0
-        framesRead = 1
-        bufSize = 1000
+        frameRead = True
+        bufSize = 1
 
-        while framesRead != 0:
+        # while we are able to read more frames, continue reading
+        while frameRead:
 
-            os.system('cls')
+            # try to read the next frame
+            ret, frame = cap.read()
+            if not ret:
+                frameRead = False
+                break
 
-            framesRead = 0
-            frames = []
-            while True:
-                print(f"Reading frame {framesRead+1}. (buffer size = {bufSize})", end = "\r")
-                ret,frame = cam.read()
-                if not ret or framesRead >= bufSize:
-                    break
-                frames.append(frame)
-                framesRead += 1
+            # prepare the frame for conversion
+            resized = cv2.resize(frame, dsize=(width, height))
+            monochromatic = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
 
-            os.system('cls')
+            # convert the frame to ascii characters
+            lst2d = [[""]*width for i in range(height)]
+            for x in range(height):
+                for y in range(width):
+                    lst2d[x][y] = to_symbol(monochromatic[x][y])
 
-            for i, frame in enumerate(frames):
-                
-                print(f"Rendering frame {i+1}/{framesRead}. Total frames rendered: {framesRendered+1}/{totalFrames-1} frames.", end = "\r")
-                resized = cv2.resize(frame, dsize=(width, height))
-                monochromatic = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
+            # format and write the converted frame to our output file
+            lines = ""
+            for lsty in lst2d:
+                line = "".join(lsty)
+                lines += line + "\t"
+            f.write(lines[:-1] + "\n")
 
-                lst2d = [[""]*width for i in range(height)]
-                for x in range(height):
-                    for y in range(width):
-                        lst2d[x][y] = to_symbol(monochromatic[x][y])
+            framesRendered += 1
 
-                lines = ""
-                for lsty in lst2d:
-                    line = "".join(lsty)
-                    lines+=line+"\t"
-                
-                f.write(lines[:-1] + "\n")
+            print(f"Total frames rendered: {framesRendered}/{totalFrames-1} frames.", end = "\r")
 
-                framesRendered += 1
-
-        cam.release()
+        cap.release()
         f.close()
 
-        os.system('cls')
+        # if the user wants the generated frames to be played, continue and play it
+        if not want_play:
+            print(f"{framesRendered} frames rendered and saved to \"{output_file}\"")
+            exit(0)
 
-        input(f"Render of \"{video}\" complete! Press enter to play... ")
+        input(f"\nRender of \"{video}\" complete! Press enter to play... ")
+        f = open(f"data\{video} output.txt", "r")
 
-        f = open(f"data/{video} output.txt", "r")
 
+    elif want_play:
+
+        if len(argv) < 3:
+            print("Incorrect number of arguments.")
+            exit(1)
+
+        if not os.path.exists(argv[2]):
+            print(f"ASCII file \"{argv[2]}\" does not exist.")
+            exit(1)
+
+        # load the text file
+        f = open(f"{argv[2]}", "r")
+        framesRendered = len(f.readlines()) - 1
+        f.seek(0)
+
+
+    # read the video's framerate
     fps_info = f.readline().split(" ")
-
     if fps_info[0] == "FRAME_RATE:":
         fps = float(fps_info[1])
 
-    os.system('cls')
-
-    # Initialize curses
     stdscr = curses.initscr()
 
     try:
         # Clear the screen
         stdscr.clear()
+        
+        # use current time to figure out which frame should be shown at any moment
+        start_time = time()
 
+        curr_frame = 0
         for i in range(framesRendered):
-            start = perf_counter()
             
+            curr_frame += 1
+
+            # read the frame from file
             frame = f.readline()
             frame = frame.replace("\t", "\n")
 
+            # display the frame
             stdscr.move(0, 0)
-            
             stdscr.addstr(frame)
             stdscr.refresh()
+            
+            # make sure that the next frame we render is the correct
+            # frame based on the time passed and the frame rate
+            # i.e. skip frames in order to get back up to speed
+            while time() - start_time >= 1/fps * curr_frame:
+                frame = f.readline()
+                curr_frame += 1
 
-            elapsed = perf_counter()-start
-            sleep(max(0,(1/fps)-elapsed))
+            # if this frame took shorter than 1/fps seconds,
+            # spin for the remaining amount of time
+            while time() - start_time <= 1/fps * curr_frame:
+                pass
 
     finally:
         # Restore the terminal to its original state
